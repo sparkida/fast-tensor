ed1ec234f711efd758072f81b87adf3a123442b2
sparkida <nick@sparkida.com>
2025-01-02 19:24:38 +0000

    Initial docs

---
+++ site/build/assets/js/49bcad06.7e2ab1f7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[742],{7417:(e,t,s)=>{s.r(t),s.d(t,{default:()=>l});s(8774);var i=s(4586),n=s(781);s(1107);var r=s(4848);function l(){const{siteConfig:e}=(0,i.A)();return(0,r.jsx)(n.A,{title:`Hello from ${e.title}`,description:"Fast tensor",children:(0,r.jsx)("main",{})})}}}]);
